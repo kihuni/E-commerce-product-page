97afbf377a93c14b1405fdabff274385b24ec726
Stephen kihuni <stephenkihuni55@gmail.com>
2021-12-30 20:36:10 +0300

    modification

--- src/Header.js
+++ src/Header.js
@@ -1,29 +1,30 @@
-import{Navbar, Container,Nav} from 'react-bootstrap'
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {cart} from '@fortawesome/fontawesome-svg-core'
+import img from '/home/stephen/ecommerce-product-page-main/src/image-avatar.png'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 const Header = () => {
     return (
-        <div>
-          <Navbar bg="light" expand="lg">
-  <Container fixed="top">
-    <Navbar.Brand href="#home">Sneakers</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Collection</Nav.Link>
-        <Nav.Link href="#link">Men</Nav.Link>
-        <Nav.Link href="#link">Women</Nav.Link>
-        <Nav.Link href="#link">About</Nav.Link>
-        <Nav.Link href="#link">Contact</Nav.Link>
-      </Nav>
-      <Nav>
-       <FontAwesomeIcon icon={cart}/>
-
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>  
+        <div className = "header">
+          <div className='burger'>
+           <div className='lineOne'></div>
+           <div className='lineTwo'></div>
+           <div className='lineThree'></div>    
+          </div>  
+          
+        <div className ="header_optionOne">
+        <div className='logo'>Sneakers</div>
+        <div className='list'>
+        <span>Collection</span>
+         <span>Men</span>
+         <span>Women</span>
+         <span>About</span>
+         <span>Contact</span>
+        </div>
+         
+         </div>
+         <div className='header_optionTwo'>
+         <AddShoppingCartIcon />
+         <img className='header_img' src={img} alt="" />
+         </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-<script src="https://use.fontawesome.com/14e4ca0bde.js"></script>
-
 
 ReactDOM.render(
   <React.StrictMode>
