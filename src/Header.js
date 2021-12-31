52a195e5e3596c8d43326af2da4cbd85e46c16be
Stephen kihuni <stephenkihuni55@gmail.com>
2021-12-31 09:34:35 +0300

    css added

--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import img from '/home/stephen/ecommerce-product-page-main/src/image-avatar.png'
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import HorizontalSplitIcon from '@mui/icons-material/HorizontalSplit';
+import React from "react";
+import img from "/home/stephen/ecommerce-product-page-main/src/image-avatar.png";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import HorizontalSplitIcon from "@mui/icons-material/HorizontalSplit";
+import "./Header.css";
 const Header = () => {
-    return (
-        <div className = "header">
-          <div className='burger'>
-           <HorizontalSplitIcon/>
-          </div>  
-          
-        <div className ="header_optionOne">
-        <div className='logo'>Sneakers</div>
-        <div className='list'>
-        <span>Collection</span>
-         <span>Men</span>
-         <span>Women</span>
-         <span>About</span>
-         <span>Contact</span>
-        </div>
-         
-         </div>
-         <div className='header_optionTwo'>
-         <AddShoppingCartIcon />
-         <img className='header_img' src={img} alt="" />
-         </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="burger">
+        <HorizontalSplitIcon />
+      </div>
 
-export default Header
+      <div className="header_optionOne">
+        <div className="logo">Sneakers</div>
+        <div className="list">
+          <span>Collection</span>
+          <span>Men</span>
+          <span>Women</span>
+          <span>About</span>
+          <span>Contact</span>
+        </div>
+      </div>
+      <div className="header_optionTwo">
+        <AddShoppingCartIcon />
+        <img className="header_img" src={img} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap');
+</style>
+
 
 ReactDOM.render(
   <React.StrictMode>
